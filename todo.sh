50cc544450e608fbe9bc37bd0c4d39f4e100aafb
Khushi <khushmish21@gmail.com>
2025-01-28 21:01:07 +0530

    check for file existence of file

--- todo.sh
+++ todo.sh
@@ -2,9 +2,14 @@
 
 if [[ "$1" == "-a" ]]; then 
     echo "to add tasks"
-    printf "this is my first task" >> todo.txt
+    if [[ -f "todo.txt" ]]; then
+        lastline= tail -n 1 todo.txt
+        echo $lastline
+        printf "this is the first task when file exists\n" >> todo.txt
+    else
+        printf "this is the first task when file doesnt exist\n" >> todo.txt
+    fi
     
-
 elif [[ "$1" == "-d" ]]; then
     echo "to delete tasks"
 
@@ -13,6 +18,5 @@ elif [[ "$1" == "-l" ]]; then
 
 else
     echo "invalid argument"
-
 fi
 
