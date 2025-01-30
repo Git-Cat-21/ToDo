4ba926f6c82e73de5ffc627c4c57a0b4dcc100b4
Git-Cat-21 <khushmish21@gmail.com>
2025-01-30 10:54:45 +0530

    done mark as done

--- todo.sh
+++ todo.sh
@@ -22,7 +22,23 @@ if [[ "$1" == "-a" ]]; then
     fi
     
 elif [[ "$1" == "-d" ]]; then
-    echo "to delete tasks"
+
+    if [[ -z "$2" ]]; then
+        read -p "Enter the task number...:   " task_no
+    else
+        task_no="$2"
+    fi
+
+    task=$(awk -v task_no="$task_no" -F'\t' '$1 == task_no {print $2}' todo.txt)
+
+    if  [[ -n "$task" ]]; then
+        echo "This is your task: $task"
+        echo "Task exists. Here we mark it as done."
+
+        sed -i "${task_no}s/$/ (done)/" todo.txt
+    else
+        echo "Task doesnt exist. So, nahhh"
+    fi
 
 elif [[ "$1" == "-l" ]]; then 
     cat todo.txt
