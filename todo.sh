658c56f85f3cd1e5a298130c33705485cafd01fe
Git-Cat-21 <khushmish21@gmail.com>
2025-01-30 09:35:37 +0530

    line numbers added

--- todo.sh
+++ todo.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 if [[ "$1" == "-a" ]]; then 
-    echo "to add tasks"
     if [[ -f "todo.txt" ]]; then
-        echo "file exists"
+        line_no=$(cat todo.txt | wc -l)
 
         if [[ -z "$2" ]]; then
-            read -p "No task entered... enter the task   " task
-            echo "$task" >> todo.txt
+            read -p "No task entered... enter the task:  " task
+            echo "$((line_no))\t$task" >> todo.txt
         else 
-            printf "$2\n" >> todo.txt
+            echo "$((line_no))\t$2\n" >> todo.txt
         fi
+        
+    else 
+        line_no = 1
+        if [[ -z "$2" ]]; then
+                read -p "No task entered... enter the task   " task
+                echo "$((line_no))\t$task" >> todo.txt
+            else 
+                echo "$((line_no))\t$2\n" >> todo.txt
+            fi
     fi
     
 elif [[ "$1" == "-d" ]]; then
     echo "to delete tasks"
 
 elif [[ "$1" == "-l" ]]; then 
-    # echo "to list tasks"
     cat todo.txt
 
 else
