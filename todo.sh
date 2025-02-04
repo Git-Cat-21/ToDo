2d26ba2cb38b7f89d56a1626e3575259133bf932
Git-Cat-21 <khushmish21@gmail.com>
2025-02-04 11:44:42 +0530

    todooo

--- todo.sh
+++ todo.sh
@@ -5,7 +5,19 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
+ascii_art="████████╗ ██████╗ ██████╗  ██████╗ 
+╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗
+   ██║   ██║   ██║██║  ██║██║   ██║
+   ██║   ██║   ██║██║  ██║██║   ██║
+   ██║   ╚██████╔╝██████╔╝╚██████╔╝
+   ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝"
+
+
+
 usage(){
+    echo "---------------------------------------"
+    echo "$ascii_art"
+    echo "---------------------------------------"
     echo "A simple command-line tool to  manage to-do list"
     echo 
     echo -e "${GREEN}Usage:${ENDCOLOR} ./todo.sh [OPTION] [<ARGUMENT>]"
@@ -13,6 +25,14 @@ usage(){
     echo "-d <task_number>      Mark task as done"      
     echo "-l                    List all tasks"
     echo "-h                    Help function"
+}
+
+Random(){
+    if [[ -f "$file"  ]]; then 
+        line_no="$(wc -l < "$file")"
+        random_number=$((1 + RANDOM % line_no))
+        echo "$random_number"
+    fi
 }
 
 if [[ "$1" == "-a" ]]; then 
@@ -59,6 +79,23 @@ elif [[ "$1" == "-l" ]]; then
 
 elif [[ "$1" == "-h" ]]; then
     usage
+
+# elif [[ "$1" == "-R" ]]; then
+#     echo " to list a random"
+#     line_number=$(Random)
+#     total_lines=$(wc -l < "$file")
+
+#     if [[ "$total_lines" -eq 0 ]]; then 
+#         echo "Error: File is Empty"
+#         exit 1
+#     fi
+
+
+#     while true; do
+#         line_number=$((RANDOM % total_lines + 1 ))
+#         selected_lines=$(sed -n "${line_number}p" "$file")
+
+#         if [[ -n "$selected_line" && ! "$selected_line" =~ DONE]]
 
 else
     echo "invalid argument"
